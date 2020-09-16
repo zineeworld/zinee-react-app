98651c626578e3cbfd234b4b9caea58bfee02811
zinee <zineeworld@gmail.com>
2020-09-16 23:52:13 +0900

    200916 day3

--- src/App.js
+++ src/App.js
@@ -1,57 +1,33 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+import axios from 'axios';
 
-function Food({ name, picture, rating }) {
-  return (
-      <div>
-        <h1>I love { name }</h1>
-        <h4>{ rating }/5.0</h4>
-        <img src={ picture } alt={ name } width="200"/>
-      </div>
-  );
-}
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies: [],
+  };
 
-const foodLike = [
-  {
-    id: 1,
-    name: 'apple',
-    image: 'https://5.imimg.com/data5/LM/DU/MY-22954806/apple-fruit-500x500.jpg',
-    rating: 5
-  },
-  {
-    id: 2,
-    name: 'banana',
-    image: 'https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg',
-    rating: 5
-  },
-  {
-    id: 3,
-    name: 'orange',
-    image: 'https://lh3.googleusercontent.com/proxy/gEzm-lrp5kP561Kl0Za9adZHqbVeND0K9xIPGrIi__Fwsey4BPVdoJd0814FI7ffZfRt9R8rahtnWRd7wgWM8yy4PTPB7qyXxxe-nnsnzuuUuA99L04V',
-    rating: 4
-  },
-  {
-    id: 4,
-    name: 'mango',
-    image: 'https://thediplomat.com/wp-content/uploads/2016/04/sizes/td-story-s-2/thediplomat_2016-04-26_19-22-13.jpg',
-    rating: 3
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      }
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+    this.setState({ movies, isLoading: false });
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
-];
 
-function App() {
-  return (
-      <div>
-        { foodLike.map(item => (
-          <Food key={item.id} name={item.name} picture={item.image} rating={item.rating}/>
-        ))}
-      </div>
-  );
+  render() {
+    const { isLoading } = this.state;
+    return (
+     <div>
+       { isLoading ? 'Loading...' : 'We are ready' }
+     </div>
+    )
+  }
 }
-
-Food.protoType = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number
-};
 
 export default App;
